f5fb354585c1cac2ad2f960ef3ccddef7b2a9ab9
YJ <yunyun3599@gmail.com>
2021-03-28 11:11:08 +0900

    studying ref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
-import EventTest from './EventTest';
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return(
-    <EventTest />
-  );
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <ScrollBox ref = {(ref) => this.scrollBox = ref}/>
+        <button onClick = {() => this.scrollBox.scrollToBottom()}>
+          맨밑으로
+        </button>
+      </div>
+    );
+  }
 };
 
 export default App;
