3ae7f3734d12aa9cc5844c4ed63e303cd9790273
YJ <yunyun3599@gmail.com>
2021-03-29 20:41:51 +0900

    studying LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
-import Advanced_Iteration from './Advanced_Iteration';
+import LifeCycle from './LifeCycle';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16); //16진수로 변환
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render(){
     return(
       <div>
-        <Advanced_Iteration/>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycle color={this.state.color}/>
       </div>
-    );
+    )
   }
 };
 
